29f81bc892e8b2ef8a70f662d4e5c34f6502e672
tuanlong1202 <tuanlong1202@gmail.com>
2021-05-28 16:49:34 -0400

    React hooks event handling lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function handleOnFocusEvent() {
+    console.log('Good!');
+}
+
+function handleOnBlurEvent() {
+    console.log('Hey! Eyes on me!');
+}
+
+function EyesOnMe() {
+    return (
+        <button onFocus={handleOnFocusEvent} onBlur={handleOnBlurEvent} >Eyes on me</button>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function handleOnChangeEvent() {
+    console.log('Entering password...');
+}
+
+function Keypad () {
+    return (
+        <input type="password" onChange={handleOnChangeEvent} />
+    );
+}
+
+export default Keypad;
